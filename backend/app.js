2b0c2633772b2beaf16e6affdc0ff9b2475b4414
Steven Kaufman <steveplaysguiola@gmail.com>
2019-10-19 18:06:17 -0500

    removed favicon things from backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const favicon = require('serve-favicon');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -26,7 +25,6 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
 
