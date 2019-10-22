77b85db8dc2d561bf2eeb8e035f79ccdcae0ae5f
Steven Kaufman <steveplaysguiola@gmail.com>
2019-10-22 14:23:59 -0500

    added todo service

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -1,32 +1,32 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 
 module.exports = function (app) {
-  const connectionString = app.get('postgres');
+  const connectionString = app.get('postgres')
   const sequelize = new Sequelize(connectionString, {
     dialect: 'postgres',
     logging: false,
     define: {
       freezeTableName: true
     }
-  });
-  const oldSetup = app.setup;
+  })
+  const oldSetup = app.setup
 
-  app.set('sequelizeClient', sequelize);
+  app.set('sequelizeClient', sequelize)
 
   app.setup = function (...args) {
-    const result = oldSetup.apply(this, args);
+    const result = oldSetup.apply(this, args)
 
     // Set up data relationships
-    const models = sequelize.models;
+    const models = sequelize.models
     Object.keys(models).forEach(name => {
       if ('associate' in models[name]) {
-        models[name].associate(models);
+        models[name].associate(models)
       }
-    });
+    })
 
     // Sync to the database
-    app.set('sequelizeSync', sequelize.sync());
+    app.set('sequelizeSync', sequelize.sync())
 
-    return result;
-  };
-};
+    return result
+  }
+}
---
+++ backend/services/todo/todo.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-sequelize')
+
+exports.Todo = class Todo extends Service {
+
+}
