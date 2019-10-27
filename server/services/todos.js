e17d19b1725a8f88059ffffc829ad756afc1e35c
Steven Kaufman <steveplaysguiola@gmail.com>
2019-10-26 21:13:58 -0500

    fixed errors; added postgres user and database

--- server/services/todos.js
+++ server/services/todos.js
@@ -3,7 +3,7 @@ const createModel = require('../models/todos.model')
 const hooks = require('../hooks/todos.hooks')
 
 module.exports = app => {
-  const Model = createModel()
+  const Model = createModel(app)
   const paginate = app.get('paginate')
 
   app.use('/todos', service({ Model, paginate }))
